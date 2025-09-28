d9ec0a35527aeaef44b558a7b7d67d85618fd5d8
gefloc <gerrit.flockenhaus@gmail.com>
2025-09-28 18:24:26 +0200

    fixed script (include-libdvdcss.sh) to faciliate DVD support

--- debian/include-libdvdcss.sh
+++ debian/include-libdvdcss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #includes the DVD encryption library from the host machine into the image
-mkdir -p config/include.chroot/usr/lib/x86_64-linux-gnu
-cp /usr/lib/x86_64-linux-gnu/libdvdcss.* config/include.chroot/usr/lib/x86_64-linux-gnu
+mkdir -p config/includes.chroot/usr/lib/x86_64-linux-gnu
+cp /usr/lib/x86_64-linux-gnu/libdvdcss.* config/includes.chroot/usr/lib/x86_64-linux-gnu
